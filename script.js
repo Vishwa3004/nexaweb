8c1466a8d327f3df698d9242b39a25f8bd80f120
Vishwajeet Dasarwad <143509223+Vishwa3004@users.noreply.github.com>
2026-03-15 14:42:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleGetStarted() {
+  alert('Welcome to NexaWeb!');
+}
+
+function handleProject() {
+  alert('Starting your project!');
+}
+
+function handleExplore() {
+  alert('Explore our work!');
+}
